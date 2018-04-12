6995a1ef8d9c08f3c5b546cfd4f728e2f0187eb7
Lucas Mafra <lucas.mafra95@gmail.com>
2018-04-11 22:11:58 -0300

    added piece

--- src/matrix.ts
+++ src/matrix.ts
@@ -2,6 +2,8 @@ export class Matrix<T> {
 
     public readonly rows: number
     public readonly cols: number
+    private INITIAL_ROW = 1
+    private INITIAL_COL = 1
     private readonly matrix: Array<Array<T>>
 
     constructor(rows: number, cols: number) {
@@ -11,22 +13,47 @@ export class Matrix<T> {
     }
 
     static of<T>(rows: number, cols: number): Matrix<T> {
+        this.validateNumberOfRowsAndCols(rows,cols)
         return new Matrix<T>(rows, cols)
     }
 
     forEach(callbackFn: (element: T, row: number, col: number) => void) {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                callbackFn(this.matrix[i][j], i+1, j+1)
+                callbackFn(this.matrix[i][j], i+this.INITIAL_ROW, j+this.INITIAL_COL)
             }
         }
     }
 
     get(row: number, col: number) {
-        return this.matrix[row-1][col-1]
+        this.validateIndex(row,col)
+        return this.matrix[row-this.INITIAL_ROW][col-this.INITIAL_COL]
     }
 
     set(element: T, row: number, col: number) {
+        this.validateIndex(row,col)
         this.matrix[row-1][col-1] = element
+    }
+
+
+    private validateIndex(row: number, col: number) {
+        if (!this.isValidIndex(row, col)) {
+            throw new Error(`Invalid index: (${row}, ${col})`)
+        }
+    }
+
+    private isValidIndex(row: number, col: number) {
+        return this.isBetweenInterval(row, this.INITIAL_ROW, this.rows) &&
+            this.isBetweenInterval(col, this.INITIAL_COL, this.cols)
+    }
+
+    private isBetweenInterval(n: number, a: number, b: number) {
+        return a <= n && b >= n
+    }
+
+    private static validateNumberOfRowsAndCols(rows: number, cols: number) {
+        if (rows < 0 || cols < 0) {
+            throw new Error('Rows and cols must be greater than or equal to 0')
+        }
     }
 }
--- src/square.ts
+++ src/square.ts
@@ -1,10 +1,44 @@
 import {ColorEnum} from "./color"
+import {Piece} from "./piece";
 
 export class Square {
 
     public readonly color: ColorEnum
+    private _piece: Piece | undefined
 
-    constructor(color: ColorEnum) {
-        this.color = color
+    constructor(color: ColorEnum)
+
+    constructor(color: ColorEnum, piece: Piece)
+
+    constructor(...args: any[]) {
+        this.color = args[0]
+        this._piece = args[1]
+    }
+
+
+    public get piece(): Piece | undefined {
+        return this._piece
+    }
+
+    public addPiece(piece: Piece) {
+        this.ensureHasNoPiece()
+        this._piece = piece
+    }
+
+    public removePiece() {
+        this.ensureHasPiece()
+        this._piece = undefined
+    }
+
+    private ensureHasNoPiece() {
+        if (this.piece) {
+            throw new Error('Already have a piece')
+        }
+    }
+
+    private ensureHasPiece() {
+        if (!this.piece) {
+            throw new Error('No piece to remove')
+        }
     }
 }
